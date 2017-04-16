9e51e6f07980f886be864e35789fb60edae45776
Matt Macy <mmacy@nextbsd.org>
2017-04-15 19:17:32 -0700

    refactor training

--- train.py
+++ train.py
@@ -1,21 +1,43 @@
 import torch
+import torch.optim as O
 import os.path
 from model import BIDAF
 from torchtext import data, datasets
 
 
+def train_batch(config, model, opt, batch):
+    model.train(); opt.zero_grad()
+    n_correct, n_total = 0, 0
+    print(batch)
+    ls, s, le, e = model(batch)
+
+
+
+def train_epoch(config, model, opt, iter):
+    iter.init_epoch()
+    n_correct, n_total = 0, 0
+    for batch_idx, batch in enumerate(iter):
+        iterations += 1
+        n_correct, n_total = train_batch(config, model, opt, batch)
+
+
 def train(config):
-    context, answer, span = data.Field(), data.Field(), data.Field()
-    train, dev, test = datasets.SQUAD.splits(context, answer, span, config=config)
     print("building context vocab")
-    context.build_vocab(train, dev)
-    train.config.word_vocab_size = len(context.vocab)
 
-    if os.path.isfile(config.vector_cache):
-        context.vocab.vectors = torch.load(config.vector_cache)
-    else:
-        context.vocab.load_vectors(wv_dir=config.data_cache, wv_type=config.word_vectors, wv_dim=config.d_embed)
-        os.makedirs(os.path.dirname(config.vector_cache), exist_ok=True)
-        torch.save(context.vocab.vectors, config.vector_cache)
+    train_iter, dev_iter, test_iter = datasets.SQUAD.iters(config,
+            batch_size=config.batch_size, device=config.gpu)
 
-    model = BIDAF(train.config)
+
+    model = BIDAF(train_iter.config)
+    model.word_embed.weight.data = context.vocab.vectors
+
+    if not config.cpu:
+        model = model.cuda()
+
+    opt = O.Adam(model.parameters())
+
+    print("training")
+    for epoch in range(config.epochs):
+        train_epoch(config, model, opt, train_iter)
+
+
